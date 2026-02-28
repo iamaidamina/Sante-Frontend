b6a8f810a6d04e51eb82f5879c68e30396c93386
Aida  Mina <iamaidamina@gmai.com>
2026-02-28 08:30:54 -0500

    finalizar primera version de medicamento

--- src/EntregasPage.jsx
+++ src/EntregasPage.jsx
@@ -28,7 +28,7 @@ function DraggableMap({ onLocationSelected }) {
     <MapContainer
       center={position}
       zoom={14}
-      style={{ height: "300px", width: "100%" }}
+      style={{ height: "300px", width: "300px" }}
     >
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
