cccd2215a8424d3adaf832f956868902e2a3efd7
carlosb114093 <78057329+carlosb114093@users.noreply.github.com>
2026-03-18 06:15:44 -0500

    Update socket.js
    New URL

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://sante-backend-production.up.railway.app", {
+const socket = io("https://sante-backend-production2.up.railway.app", {
   autoConnect: false
 });
 
